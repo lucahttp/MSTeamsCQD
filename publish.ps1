e8b21cc0ae369067bb7a538c15a200248899edaf
luca.sain.ext@boehringer-ingelheim.com <luca.sain.ext@boehringer-ingelheim.com>
2022-07-12 12:31:21 -0300

    1.2.1

--- publish.ps1
+++ publish.ps1
@@ -7,42 +7,10 @@ param (
 )
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$srcPath = "$scriptPath\src";
+$srcPath = "$scriptPath";
 Write-Host "Proceeding to publish all code found in $srcPath"
 
-$outFile = "$scriptPath\$ModuleName\$ModuleName.psm1"
-if (Test-Path $outFile) 
-{
-    Remove-Item $outFile
-}
-
-if (!(Test-Path "$scriptPath\$ModuleName")) 
-{
-    New-Item "$scriptPath\$ModuleName" -ItemType Directory
-}
-
-$ScriptFunctions = @( Get-ChildItem -Path $srcPath\*.ps1 -ErrorAction SilentlyContinue -Recurse )
-$ModulePSM = @( Get-ChildItem -Path $srcPath\*.psm1 -ErrorAction SilentlyContinue -Recurse )
-foreach ($FilePath in $ScriptFunctions) {
-    $Results = [System.Management.Automation.Language.Parser]::ParseFile($FilePath, [ref]$null, [ref]$null)
-    $Functions = $Results.EndBlock.Extent.Text
-    $Functions | Add-Content -Path $outFile
-}
-foreach ($FilePath in $ModulePSM) {
-    $Content = Get-Content $FilePath
-    $Content | Add-Content -Path $outFile
-}
-"Export-ModuleMember -Function * -Cmdlet *" | Add-Content -Path $outFile
-
-# Now replace version in psd1
-
-# Cannot find a tool in the manifest file that has a command named 'dotnet-gitversion'.
-# $fileContent = Get-Content "$scriptPath\$ModuleName.psd1.source"
-# $fileContent = $fileContent -replace '{{version}}', $version
-# $fileContent = $fileContent -replace '{{preReleaseTag}}', $preReleaseTag 
-# Set-Content "$scriptPath\$ModuleName\$ModuleName.psd1" -Value $fileContent -Force
-
 Publish-Module `
-    -Path $scriptPath\$ModuleName `
+    -Path $scriptPath `
     -NuGetApiKey $apiKey `
     -Verbose -Force
